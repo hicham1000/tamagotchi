1dd40c3db2167740a3899f2ad9b5d0d15bb83c27
Antoine Bluchet <soyuka@users.noreply.github.com>
2021-09-28 16:49:06 +0200

    Correction

--- index.js
+++ index.js
@@ -1,100 +1,87 @@
-const readline = require('readline');
-const logUpdate = require('log-update')
-const { between, generateRandomSpace } = require('./utils');
-const { stat } = require('fs');
+const readline = require("readline");
+const logUpdate = require("log-update");
+const { between, generateRandomSpace } = require("./utils");
+const { stat } = require("fs");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-}) 
+  input: process.stdin,
+  output: process.stdout,
+});
 
 readline.emitKeypressEvents(process.stdin, rl);
-    if (process.stdin.isTTY){
-        process.stdin.setRawMode(true);
-    }
-function startnewlife (){
-    
-    state.life = 0
+if (process.stdin.isTTY) {
+  process.stdin.setRawMode(true);
+}
+function startnewlife() {
+  state.life = 0;
 }
 
-rl.on('close', () => {
-    process.exit(0)
-  })
+rl.on("close", () => {
+  process.exit(0);
+});
 
-  const bear = [
-    'ʕ•ᴥ•ʔ',
-    'ʕ·ᴥ·ʔ',
-    'ʕºᴥºʔ'
-];
+const bear = ["ʕ•ᴥ•ʔ", "ʕ·ᴥ·ʔ", "ʕºᴥºʔ"];
 
 //Etat
 const state = {
-    life :20, 
-    time:0,  //temps en secondes
-    chanter :false
-}
+  life: 20,
+  time: 0, //temps en secondes
+  chanter: false,
+};
 
 function getOurs() {
-    return generateRandomSpace() + bear[Math.floor(Math.random() * bear.length)]
+  return generateRandomSpace() + bear[Math.floor(Math.random() * bear.length)];
 }
 
 function getmusic() {
-    const nbmusic = Math.floor((30*state.life)/100)
-    const music = new Array(nbmusic).fill("♫♩♬ ◙▒◙ ").join("")
-    return music
+  const nbmusic = Math.floor((30 * state.life) / 100);
+  const music = new Array(nbmusic).fill("♫♩♬ ◙▒◙ ").join("");
+  return music;
 }
 
 function getLifeBar() {
-    const nbcoeur = Math.floor((30*state.life)/100)
-    const vie = new Array(nbcoeur).fill("❤").join("")
+  const nbcoeur = Math.floor((30 * state.life) / 100);
+  const vie = new Array(nbcoeur).fill("❤").join("");
 
-    return vie 
-    // if (state.life < 60){
-    //     return "❤❤❤"
-    // }
-    // return "❤❤❤❤❤❤ " + state.life + "/100"
-    // if (state.life < 30){
-    //     return "❤❤❤"
-    // }
-    // return "❤❤❤❤❤❤❤ " + state.life + "/100"
+  return vie;
+  // if (state.life < 60){
+  //     return "❤❤❤"
+  // }
+  // return "❤❤❤❤❤❤ " + state.life + "/100"
+  // if (state.life < 30){
+  //     return "❤❤❤"
+  // }
+  // return "❤❤❤❤❤❤❤ " + state.life + "/100"
 
-    // if (state.life < 70){
-    //        return "🍔ԅ( ͒ ۝ ͒ )🍔ԅ( ͒ ۝ ͒ )🍔ԅ  ❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤ " +state.life + "/100"   
-    //    }
-    //    return "❤❤❤"   
-    //    if (state.life < 70){
-    //     return "◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤" +state.life + "/100"   
-    // }
-    // return "❤❤❤❤❤❤"   
+  // if (state.life < 70){
+  //        return "🍔ԅ( ͒ ۝ ͒ )🍔ԅ( ͒ ۝ ͒ )🍔ԅ  ❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤ " +state.life + "/100"
+  //    }
+  //    return "❤❤❤"
+  //    if (state.life < 70){
+  //     return "◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ◙▒◙ ♫♩♬ ❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤" +state.life + "/100"
+  // }
+  // return "❤❤❤❤❤❤"
 }
-setInterval(function(){
-    const espace =[
-        getOurs(),
-        '',
-        getLifeBar() + '  ' + getmusic()
-    ]
-    logUpdate(espace.join('\n'))
-}, 70)
+setInterval(function () {
+  const espace = [getOurs(), "", getLifeBar() + "  " + getmusic()];
+  logUpdate(espace.join("\n"));
+}, 70);
 
 //boucle d'etat
-setInterval(function() {
-    state.time += 1  
+setInterval(function () {
+  state.time += 1;
 
-    if (state.life === 100) {  
-        return;
+  if (state.life === 100) {
+    return;
+  }
 
-    }
+  if (state.time % 3 === 0) {
+    state.life++;
+  }
+}, 100);
 
-    if (state.time % 3 === 0){
-        state.life++       
-    }
-  
-},100)
-process.stdin.on('keypress', (character, key) => {
-    
-    if(key.name === 'm') {
-        startnewlife();
-        
-        return startnewlife();
-    }
-})
+process.stdin.on("keypress", (character, key) => {
+  if (key.name === "m") {
+    startnewlife();
+  }
+});
